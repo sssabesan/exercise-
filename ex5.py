898c25af3c019757ad4ffe765397431a0b44d14e
Sabesan <sssabesan@gmail.com>
2025-08-10 10:21:19 +0530

    while loop ex
    class exercise in while loop

---
+++ ex5.py
@@ -0,0 +1,34 @@
+'''
+x=1
+while x<=10:
+    print(x,end=",")
+    x=x+2
+print("\n")
+
+x=2
+while x<=10:
+    print(x,end=",")
+    x=x+2   
+print("\n")    
+x=1
+y=2
+while x<=15:
+    print(x,end=",")
+    x=(x+y)
+    y=y+1
+print("\n")
+x=1
+y=1
+while x<=25:
+    print(x,end=",")
+    x=(y+1)**2
+    y+=1
+print("\n")
+'''
+question5
+x=1
+while x<=0:
+    x%2!=0
+    print(x,end=",")
+    x=x+1
+    print("*"
